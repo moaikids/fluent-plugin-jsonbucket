b64f2f20dea7f296887b2ce3f201227a6b1debc2
moaikids <moaikids@gmail.com>
2013-08-28 20:35:23 +0900

    first commit

---
+++ lib/fluent/plugin/out_jsonbucket.rb
@@ -0,0 +1,21 @@
+module Fluent
+    class JsonbucketOutput < Fluent::Output
+        Fluent::Plugin.register_output('jsonbucket', self)
+        config_param :output_tag, :string, :default => nil
+        config_param :json_key, :string, :default => 'json'
+
+        def configure(conf)
+            super
+            unless config.has_key?('output_tag')
+                raise Fluent::ConfigError, "you must set 'output_tag'"
+            end
+        end
+
+        def emit(tag, es, chain)
+            es.each {|time,record|
+                bucket = {@json_key => record.to_json}
+                Fluent::Engine.emit(@output_tag, time, bucket)
+            }
+        end 
+    end
+end
